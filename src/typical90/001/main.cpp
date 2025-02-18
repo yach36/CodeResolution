bd60e131b2df1b7efa2d1085c50a126131158b66
yasuda.chuma <86759515+yach36@users.noreply.github.com>
2025-02-18 22:39:00 +0900

    initial commit

---
+++ src/typical90/001/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+
+int main() {
+  int n;
+  cin >> n;
+
+  int ans = 0;
+
+  cout << ans << endl;
+  return 0;
+}
