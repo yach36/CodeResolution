623a979a35e7864db0336506e931610d381bd8e0
yach36 <86759515+yach36@users.noreply.github.com>
2025-03-25 01:45:37 +0900

    leetcode zigzag-conversion resolved

--- src/LeetCode/zigzag-conversion/main.cpp
+++ src/LeetCode/zigzag-conversion/main.cpp
@@ -4,75 +4,41 @@ using namespace std;
 class Solution {
  public:
   string convert(string s, int numRows) {
-    vector<queue<char>> A(1000);
-
-    int col = 0;
-    {
-      int i = 0;
-      bool finished = false;
-      while (true) {
-        // numRows文字埋めるモード
-        for (int j = 0; j < numRows; j++) {
-          A.at(col).push(s.at(i));
-
-          i++;
-          if (i >= int(s.size())) {
-            finished = true;
-            break;
-          }
-        }
-        if (finished) {
-          break;
-        }
-        col++;
-
-        // 斜め方向に文字を配置するモード
-        for (int j = numRows - 2; j > 0; j--) {
-          for (int k = 0; k < numRows; k++) {
-            if (k == j) {
-              A.at(col).push(s.at(i));
-            } else {
-              A.at(col).push('#');
-            }
-          }
-
-          i++;
-          if (i >= int(s.size())) {
-            finished = true;
-            break;
-          }
-          col++;
-        }
-        if (finished) {
-          break;
-        }
-      }
+    if (numRows == 1 || numRows >= int(s.size())) {
+      return s;
     }
 
-    // 出力
-    string ans;
-    for (int i = 0; i < numRows; i++) {
-      for (int j = 0; j <= col; j++) {
-        char front = A.at(j).front();
-        if (front != '#') {
-          ans.push_back(front);
-        }
+    int unit = 2 * (numRows - 1);
+    int numGroups = (int(s.size()) - 1) / unit + 1;
+    vector<queue<string>> A(numGroups);
 
+    for (int i = 0; i < int(s.size()); i += unit) {
+      int group = i / unit;
+      A.at(group).push({s.at(i)});
+      if (i + 1 >= int(s.size())) {
+        break;
+      }
+
+      for (int l = i + 1, r = (group + 1) * unit - 1;
+           r - l >= 0 && l < int(s.size()); l++, r--) {
+        if (r == l || r >= int(s.size())) {
+          A.at(group).push({s.at(l)});
+        } else {
+          A.at(group).push({s.at(l), s.at(r)});
+        }
+      }
+    };
+
+    string ans = "";
+    const int size = int(A.at(0).size());
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < int(A.size()); j++) {
         if (!A.at(j).empty()) {
+          ans += A.at(j).front();
           A.at(j).pop();
         }
       }
     }
-
     return ans;
   }
 };
-
-int main() {
-  auto sl = new Solution();
-  string s;
-  int n;
-  cin >> s >> n;
-  string ans = sl->convert(s, n);
-  cout << ans << endl;
-}
