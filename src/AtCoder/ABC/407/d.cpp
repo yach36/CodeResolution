c68356458ba9dfa27a9240e1ea8910896e8b815d
yach36 <86759515+yach36@users.noreply.github.com>
2025-06-01 12:09:28 +0900

    WIP 407 d

---
+++ src/AtCoder/ABC/407/d.cpp
@@ -0,0 +1,30 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//   int h, w;
+//   cin >> h >> w;
+
+//   int a[h][w];
+
+//   for (int i = 0; i < h; i++) {
+//     for (int j = 0; j < w; j++) {
+//       cin >> a[i][j];
+//     }
+//   }
+
+//   // マスではなく罫線を1要素とした配列を作る
+//   // 上下左右に隣接するドミノは共存できない
+
+//   //   bitset<20> bs;
+//   int n = (h - 1) * (w - 1);
+//   for (int b = 0; b < (1 << n); b++) {
+//     int ans = -1;
+//     for (int i = 0; i < n; i++) {
+//       if (b & (1 << i)) {
+
+        
+//       }
+//     }
+//   }
+// }
